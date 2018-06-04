94353e34d12c70b4d22cf291be9d1563f83d5d71
mar <thayshi@yandex.ru>
2018-06-04 21:06:30 +0300

    add selection sort on python

---
+++ selection_sort/selectionSort.py
@@ -0,0 +1,18 @@
+def findSmallest(arr): # searching smallest element in array
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectionSort([5,8,99,4,1,0]))
+print(selectionSort(["f","yy","abc","a","y"]))
