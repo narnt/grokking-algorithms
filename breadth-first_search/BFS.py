83b19410e56f006fe807ef601f4990e5f953c767
mar <thayshi@yandex.ru>
2018-06-12 17:36:27 +0300

    add bfs on py

---
+++ breadth-first_search/BFS.py
@@ -0,0 +1,27 @@
+graph = {}
+graph["you"]=["alice","bob","claire"]
+graph["bob"]=["anuj","peggy"]
+graph["alice"]=["peggy"]
+graph["claire"]=["thom","jonny"]
+graph["anuj"]=[]
+graph["thom"]=[]
+graph["jonny"]=[]
+
+from collections import deque
+
+def search(name): #search person with'y' in person's name
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if 'y' in person:
+                print("it is %s"%person)
+                return True
+            else:
+                search_queue += graph[person]
+                print(person)
+                searched.append(person)
+                
+search("you")
