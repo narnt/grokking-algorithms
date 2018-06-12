8202e2af31c511e4d5676ce35c6327690a2d8d9f
narnt <32932910+narnt@users.noreply.github.com>
2018-06-12 17:22:38 +0300

    Update BFS.py

--- breadth-first_search/BFS.py
+++ breadth-first_search/BFS.py
@@ -23,9 +23,5 @@ def search(name): #search person with'y' in person's name
                 search_queue += graph[person]
                 print(person)
                 searched.append(person)
-                # return False
-
-
-# persons = {'me': [{'you': ['Clair', {'Peggie': 'Amy'}, {'Clair': ['Marine', 'Iris']}, {'Ann': 'Beka'}}]}
-# d = {'dict': ['Clair', {'Peggie': 'Am'}, {'Clair': ['Marine', 'Iris']}, {'Ann': 'Beka'}], 'dictionary': 2}
+                
 search("you")
